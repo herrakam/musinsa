b4ee5170eff9b6f801ab678fc0ee847b722753b7
SeungHyun <fm10033@gmail.com>
2022-07-14 12:07:33 +0900

    Feat : global style 적용, 필요한 이미지 다운로드

---
+++ musinsa/src/design/globalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+export const mixin = {
+  flexbox: ({ dir = 'row', horizontal = '', vertical = '' }) => `
+    display: flex;
+    flex-direction: ${dir};
+    justify-content: ${horizontal};
+    align-items: ${vertical};
+    `,
+};
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    * {
+        background: none;
+        border:none;
+        box-sizing:border-box;
+        margin:0;
+        padding:0;
+        font-family: 'Apple SD Gothic Neo';
+        font-style: normal;
+}
+`;
+export default GlobalStyles;
---
+++ musinsa/src/design/theme.js
@@ -0,0 +1,38 @@
+const backgroundColors = {
+  white: '#FFFFFF',
+  gray1: '#EEFEEE',
+  blue1: ' #0078FF',
+};
+const fontColors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  red: '#FF0000',
+  lightGray: '#AAAAAA',
+  blue1: ' #0078FF',
+};
+const fontStyles = {
+  filter: {
+    fontWeight: 400,
+    fontSize: '14px',
+  },
+  label: {
+    fontWeight: 400,
+    fontSize: '12px',
+  },
+  brandTitle: {
+    fontWeight: 400,
+    fontSize: '11px',
+  },
+  productTitle: {
+    fontWeight: 700,
+    fontSize: '14px',
+  },
+  finalPriceAndSale: {
+    fontWeight: 500,
+    fontSize: '16px',
+  },
+  originPrice: {
+    fontWeight: 500,
+    fontSize: '11px',
+  },
+};
