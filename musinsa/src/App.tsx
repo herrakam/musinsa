f2c63e240fd1f35836331589b575a6587fe9253d
SeungHyun <fm10033@gmail.com>
2022-07-15 10:47:56 +0900

    Feat : React-query, recoil 설치

--- musinsa/src/App.tsx
+++ musinsa/src/App.tsx
@@ -2,12 +2,15 @@ import Header from 'components/Header';
 import { theme } from 'design/theme';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'design/globalStyle';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header />
+      <RecoilRoot>
+        <Header />
+      </RecoilRoot>
     </ThemeProvider>
   );
 }
